0c1185f0dc7c45bf4b00791b278ba7b1fbaed6a1
MeltingPPot <miaoting666666@163.com>
2026-09-04 19:07:43 +0800

    更新了字符串

---
+++ templates/字符串/字符串哈希.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using ull = unsigned long long;
+ull base = 131;
+ull mod1 = 212370440130137957, mod2 = 1e9 + 7;
+
+ull get_hash1(std::string s) {
+  int len = s.size();
+  ull ans = 0;
+  for (int i = 0; i < len; i++) ans = (ans * base + (ull)s[i]) % mod1;
+  return ans;
+}
+
+ull get_hash2(std::string s) {
+  int len = s.size();
+  ull ans = 0;
+  for (int i = 0; i < len; i++) ans = (ans * base + (ull)s[i]) % mod2;
+  return ans;
+}
+
+bool cmp(const std::string s, const std::string t) {
+  bool f1 = get_hash1(s) != get_hash1(t);
+  bool f2 = get_hash2(s) != get_hash2(t);
+  return f1 || f2;
+}
+
+// 双哈希
